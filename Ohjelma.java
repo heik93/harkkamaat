beb882b777431b93281600feb5623e4bba65d25d
heik93 <htmkos@utu.fi>
2016-03-13 22:02:51 +0200

    update Ohjelma.java
    lisatty kaikilli olioille omat tallennusmetodit

--- Ohjelma.java
+++ Ohjelma.java
@@ -4,9 +4,37 @@ public class Ohjelma {
 		
 	}
 	
-	public void tallennaTiedot(){
+	public void tallennaTiedotAsiakas(){
 		try{
-			PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+			PrintWriter writer = new PrintWriter("Asiakkaat.txt", "UTF-8");
+			writer.println("The first line");
+			for(int i = 0;i<3;i++){
+				
+			}
+			writer.println("The last line");
+			writer.close();
+		}
+			catch(IOException e){
+				
+			}
+	}
+	public void tallennaTiedotVaraus(){
+		try{
+			PrintWriter writer = new PrintWriter("Varaukset", "UTF-8");
+			writer.println("The first line");
+			for(int i = 0;i<3;i++){
+				
+			}
+			writer.println("The last line");
+			writer.close();
+		}
+			catch(IOException e){
+				
+			}
+	}
+	public void tallennaTiedotMatkat(){
+		try{
+			PrintWriter writer = new PrintWriter("Matkat.txt", "UTF-8");
 			writer.println("The first line");
 			for(int i = 0;i<3;i++){
 				
