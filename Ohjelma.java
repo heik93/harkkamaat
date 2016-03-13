81b0f96d314fa0da76f010ae3563eb151cd2acad
heik93 <htmkos@utu.fi>
2016-03-13 22:36:26 +0200

    Update Ohjelma.java
    Ohjelman alku joka luo teksti tiedostot jos niita ei aikaisemmin ollut

--- Ohjelma.java
+++ Ohjelma.java
@@ -1,49 +1,45 @@
+import java.io.*;
+
 public class Ohjelma {
 	
 	public static void main(String[] args) {
+		File f = new File("Asiakkaat.txt");
+		if((f.exists() && !f.isDirectory()) == false) { 
+		
+			try{
+				PrintWriter writer = new PrintWriter("Asiakkaat.txt", "UTF-8");
+				writer.close();
+			}
+				catch(IOException e){
+				
+			}
+		}
+		File g = new File("Varaukset.txt");
+		if((g.exists() && !g.isDirectory()) == false) { 
+		
+			try{
+				PrintWriter writer = new PrintWriter("Varaukset.txt", "UTF-8");
+				writer.close();
+			}
+				catch(IOException e){
+				
+			}
+		}
+		File h = new File("Matkat.txt");
+		if((h.exists() && !h.isDirectory()) == false) { 
+		
+			try{
+				PrintWriter writer = new PrintWriter("Matkat.txt", "UTF-8");
+				writer.close();
+			}
+				catch(IOException e){
+				
+			}
+		}
+		
+		
+		
 		
 	}
 	
-	public void tallennaTiedotAsiakas(){
-		try{
-			PrintWriter writer = new PrintWriter("Asiakkaat.txt", "UTF-8");
-			writer.println("The first line");
-			for(int i = 0;i<3;i++){
-				
-			}
-			writer.println("The last line");
-			writer.close();
-		}
-			catch(IOException e){
-				
-			}
-	}
-	public void tallennaTiedotVaraus(){
-		try{
-			PrintWriter writer = new PrintWriter("Varaukset", "UTF-8");
-			writer.println("The first line");
-			for(int i = 0;i<3;i++){
-				
-			}
-			writer.println("The last line");
-			writer.close();
-		}
-			catch(IOException e){
-				
-			}
-	}
-	public void tallennaTiedotMatkat(){
-		try{
-			PrintWriter writer = new PrintWriter("Matkat.txt", "UTF-8");
-			writer.println("The first line");
-			for(int i = 0;i<3;i++){
-				
-			}
-			writer.println("The last line");
-			writer.close();
-		}
-			catch(IOException e){
-				
-			}
-	}
 }
