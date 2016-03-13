a09abb7a51aac975f8f7e4d7631a8a7986b1ef8d
heik93 <htmkos@utu.fi>
2016-03-13 21:48:01 +0200

    lisatty tietojen tallentamista varten metodin runko

--- Ohjelma.java
+++ Ohjelma.java
@@ -3,4 +3,19 @@ public class Ohjelma {
 	public static void main(String[] args) {
 		
 	}
+	
+	public void tallennaTiedot(){
+		try{
+			PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+			writer.println("The first line");
+			for(int i = 0;i<3;i++){
+				
+			}
+			writer.println("The last line");
+			writer.close();
+		}
+			catch(IOException e){
+				
+			}
+	}
 }
